a7d8d0ef3345f5f4ecbee1191c24ba8b26af17f5
DavidFFDAW <davidferflo2@gmail.com>
2021-01-10 02:24:39 +0100

    feat(test): adding some more test to see how it behaves

--- reactingMessage/test.js
+++ reactingMessage/test.js
@@ -1,4 +1,11 @@
 const user = 'Francisco Jones';
-const salute = name => console.log(`Bienvenido a esta app ${name}, ¿que tal?`);
+const array = ['✂️','🪨','🧻'];
 
+const salute = name => console.log(`Bienvenido a esta app ${name}, ¿que tal?`);
+const getRandomTill = num => Math.floor(Math.random() * num);
+const write = what => console.log(what);
+
+write(array[0]);
+write(array[1]);
+write(array[2]);
 salute(user);
